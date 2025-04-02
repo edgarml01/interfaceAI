01357850c385bb16240f12afd321aabe6e534884
Edgar M <egarmola08@gmail.com>
2025-04-01 20:47:24 -0600

    Agregando libreria para detectar similitud semantica

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def calculate_cosine_similarity(sentence1, sentence2):
@@ -11,11 +12,24 @@ def calculate_cosine_similarity(sentence1, sentence2):
     return cosine_sim[0][0]
 # Oraciones a comparar
 
-sentence1 = "Me gusta programar en python"
-sentence2 = "Me gusta programar"
+sentence1 = "Presiona el boton continuar"
+sentence2 = "Presiona el boton para avazar"
 
 similitud = calculate_cosine_similarity(sentence1, sentence2)
+print("Similitud Coseno sin similitud semantica:", similitud)
 
-print("Similitud Coseno:", similitud)
+model = SentenceTransformer("all-MiniLM-L6-v2")  # Modelo ligero pero preciso
+
+# Frases a comparar
+sentences = ["Presiona el botón continuar", "Presiona el botón para avanzar"]
+
+# Convertir frases en vectores numéricos
+embeddings = model.encode(sentences)
+
+# Calcular similitud coseno
+cosine_sim = cosine_similarity([embeddings[0]], [embeddings[1]])
+
+print("Similitud Coseno con semantica:", cosine_sim[0][0])
+
 
 
